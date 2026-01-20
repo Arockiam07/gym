18de8e9d042bd933afd9d6f75544dba97fa66acc
kevin Arockiam <kevinarockiam806@gmail.com>
2026-01-20 09:35:31 +0530

    update

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Contacts from '../components/ls'
+import Info from '../components/Info'
+
 
 function Contact() {
   return (
     <div>
-      <Contacts/>
+     <Info />
     </div>
   )
 }
